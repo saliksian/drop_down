42506ba63c1a3939ce437a37fed41b78a42853e1
Muhammad Salik <75317422+saliksian@users.noreply.github.com>
2022-07-21 09:42:11 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,40 +1,26 @@
 import "./App.css";
 import { useState } from "react";
-import Inputs from "./Inputs";
+import Multiselect from "multiselect-react-dropdown";
 
 function App() {
-  const optionArray = ["Option1", "Option2", "Option3", "Option4", "Option5"];
+  
+  const handleSelect = (value)=>{
 
-  const handleOnChange = (position) => {
-    const updatedCheckedState = checkedState.map((item, index) =>
-      index === position ? !item : item
-    );
-    setCheckedState(updatedCheckedState);
-  };
-
-  const [checkedState, setCheckedState] = useState(
-    new Array(optionArray.length).fill(false)
-  );
+  }
 
   return (
-    <div className="App">
-      <h1>Select Your Choice</h1>
-      <div>
-        {optionArray.map((option, index) => {
-          return (
-            <div key={index}>
-              <input
-                className="input"
-                type="checkbox"
-                checked={checkedState[index]}
-                onChange={() => handleOnChange(index)}
-              />
-              <label className="text">{option}</label>
-              {checkedState[index] === true ? <Inputs></Inputs> : null}
-            </div>
-          );
-        })}
-      </div>
+    <div>
+      <Multiselect
+        isObject={false}
+        // onKeyPressFn={function noRefCheck() {}}
+        onRemove={function noRefCheck() {}}
+        onSearch={function noRefCheck() {}}
+        onSelect={(e)=>handleSelect(e)}
+        options={["Option 1", "Option 2", "Option 3", "Option 4", "Option 5"]}
+      />
+
+    
+
     </div>
   );
 }
