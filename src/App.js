92c26771206437bd2007f4bee1cf23bac1ef6b6f
Muhammad Salik <75317422+saliksian@users.noreply.github.com>
2022-07-21 07:46:10 -0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Inputs from "./Inputs";
 
 function App() {
+  const optionArray = ["Option1", "Option2", "Option3", "Option4", "Option5"];
+
+  const handleOnChange = (position) => {
+    const updatedCheckedState = checkedState.map((item, index) =>
+      index === position ? !item : item
+    );
+    setCheckedState(updatedCheckedState);
+  };
+
+  const [checkedState, setCheckedState] = useState(
+    new Array(optionArray.length).fill(false)
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Select Your Choice</h1>
+      <div>
+        {optionArray.map((option, index) => {
+          return (
+            <div key={index}>
+              <input
+                className="input"
+                type="checkbox"
+                checked={checkedState[index]}
+                onChange={() => handleOnChange(index)}
+              />
+              <label className="text">{option}</label>
+              {checkedState[index] === true ? <Inputs></Inputs> : null}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
