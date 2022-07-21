45a9202969b9a073ef5da0f8229782dafe2826c4
Muhammad Salik <75317422+saliksian@users.noreply.github.com>
2022-07-21 10:10:17 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -3,24 +3,50 @@ import { useState } from "react";
 import Multiselect from "multiselect-react-dropdown";
 
 function App() {
-  
-  const handleSelect = (value)=>{
+  const [optionSelected, setOptionSelected] = useState([]);
 
-  }
+  const handleSelect = (value) => {
+    setOptionSelected([...value]);
+  };
+  const handleRemove = (value) => {
+    setOptionSelected([...value]);
+  };
 
   return (
     <div>
       <Multiselect
         isObject={false}
         // onKeyPressFn={function noRefCheck() {}}
-        onRemove={function noRefCheck() {}}
+        onRemove={(e) => handleRemove(e)}
         onSearch={function noRefCheck() {}}
-        onSelect={(e)=>handleSelect(e)}
+        onSelect={(e) => handleSelect(e)}
         options={["Option 1", "Option 2", "Option 3", "Option 4", "Option 5"]}
       />
 
-    
-
+      {optionSelected.map((value, index) => {
+        return (
+          <div key={index}>
+            <h1>{value}</h1>
+            <input type="text"></input>
+            <div>
+              <Multiselect
+                isObject={false}
+                // onKeyPressFn={function noRefCheck() {}}
+                onRemove={function noRefCheck() {}}
+                onSearch={function noRefCheck() {}}
+                onSelect={function noRefCheck() {}}
+                options={[
+                  "Value 1",
+                  "Value 2",
+                  "Value 3",
+                  "Value 4",
+                  "Value 5",
+                ]}
+              />
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
